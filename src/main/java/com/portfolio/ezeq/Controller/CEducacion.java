f888adb21c01bb3d51c60c2b07b3fbb7ecafd57d
ezequiel <ezediaz_00@hotmail.com>
2023-05-13 17:33:05 -0300

    corrigiendo cors

--- src/main/java/com/portfolio/ezeq/Controller/CEducacion.java
+++ src/main/java/com/portfolio/ezeq/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendeze.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portfolio/ezeq/Controller/PersonaController.java
+++ src/main/java/com/portfolio/ezeq/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendeze.web.app")
 @RequestMapping("/personas")
 public class PersonaController {
    @Autowired
