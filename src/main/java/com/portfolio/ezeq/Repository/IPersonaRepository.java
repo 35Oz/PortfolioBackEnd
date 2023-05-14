06fe37b49fa1d3c5ac139b4b20db997e20f88a25
ezequiel <ezediaz_00@hotmail.com>
2023-05-14 03:19:56 -0300

    subida sin h2

--- src/main/java/com/portfolio/ezeq/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/ezeq/Repository/IPersonaRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface IPersonaRepository extends JpaRepository<Persona, Integer>{
     public Optional<Persona> findByNombre(String nombre);
     public boolean existsByNombre(String nombre);
+
+ 
 }
