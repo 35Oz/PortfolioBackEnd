d6374dacca7696ecb4a18bae029077204701c685
ezequiel <ezediaz_00@hotmail.com>
2023-05-13 23:48:53 -0300

    funcion nueva persona

--- src/main/java/com/portfolio/ezeq/Controller/PersonaController.java
+++ src/main/java/com/portfolio/ezeq/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,13 @@ public class PersonaController {
         List<Persona> list = personaService.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
+    
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+        personaService.save(persona);
+        return "la persona fue creada correctamente";
+    }
+    
     
     @GetMapping("/detail/{id}")
     public ResponseEntity<Persona> getById(@PathVariable("id")int id){
